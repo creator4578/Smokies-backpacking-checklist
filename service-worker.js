4ec119af916b84912be9ef15b053fff87cabe8f4
creator4578 <127627750+creator4578@users.noreply.github.com>
2026-08-31 23:36:47 -0500

    Upgrade checklist to trip dashboard

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='appalachian-run-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
