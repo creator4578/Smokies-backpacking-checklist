e21c47c4a88b3f9986448756ef668dc97a7310a7
creator4578 <127627750+creator4578@users.noreply.github.com>
2026-09-05 21:24:52 -0500

    Update packing checklist and food plan

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='appalachian-run-v1';
+const CACHE='appalachian-run-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
